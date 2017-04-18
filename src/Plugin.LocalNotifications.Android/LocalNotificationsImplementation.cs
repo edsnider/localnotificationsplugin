4401f58a5a15d1fe5e19c7e6235cf5e45bddc668
roberto <roberto.can@gmail.com>
2017-04-18 14:50:09 -0300

    Add extra key called "NotificationId" and its value to create the possibility to override the method "OnNewIntent" and check this key and value.

--- src/Plugin.LocalNotifications.Android/LocalNotificationsImplementation.cs
+++ src/Plugin.LocalNotifications.Android/LocalNotificationsImplementation.cs
@@ -19,6 +19,11 @@ namespace Plugin.LocalNotifications
         public static int NotificationIconId { get; set; }
 
         /// <summary>
+        /// The key used to Intent's Extra.
+        /// </summary>
+        public const string LocalNotificationIntentKey = "NotificationId";
+
+        /// <summary>
         /// Show a local notification
         /// </summary>
         /// <param name="title">Title of the notification</param>
@@ -42,6 +47,7 @@ namespace Plugin.LocalNotifications
 
             var resultIntent = GetLauncherActivity();
             resultIntent.SetFlags(ActivityFlags.NewTask | ActivityFlags.ClearTask);
+            resultIntent.PutExtra(LocalNotificationIntentKey, id);
             var stackBuilder = Android.Support.V4.App.TaskStackBuilder.Create(Application.Context);
             stackBuilder.AddNextIntent(resultIntent);
             var resultPendingIntent =
