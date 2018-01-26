549471429f30f2794aa7be129181474e06c05b9e
Ed Snider <ed.snider@infernored.com>
2018-01-26 18:07:37 -0500

    added appveyor config and updated nuspec updated AssemblyInfo files in all projects

--- src/Plugin.LocalNotifications.macOS/Properties/AssemblyInfo.cs
+++ src/Plugin.LocalNotifications.macOS/Properties/AssemblyInfo.cs
@@ -1,27 +1,6 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-
-// Information about this assembly is defined by the following attributes. 
-// Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("Plugin.LocalNotifications")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Plugin.LocalNotifications")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2.0")]
-[assembly: AssemblyFileVersion("2.2.0")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
+[assembly: AssemblyVersion("1.0.0")]
